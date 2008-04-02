fa382b33e666784f6e6fe5d9658d53b4082486c1
otsune <otsune@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-02 08:45:39 +0000

    vimperator-plugins/pukka.js: Add configurable
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@8654 d0d07461-0603-4401-acd4-de1884942a52

--- pukka.js
+++ pukka.js
@@ -4,25 +4,44 @@
  * For liberator 0.6pre
  * @author otsune (based on teramako)
  * @version 0.1
+ * 
+ * Variable:
+ *  g:pukka_normalizelink
+ *      Specifies keys that use Pathtraq URL Normalizer 
+ *      usage: let g:pukka_normalizelink = "true"
+ * Mappings:
+ *  '[C-z]':
+ * Commands:
+ *  'pukka' or 'pu':
+ *      Post bookmark to del.icio.us with Pukka
+ *      usage: :pukka http://example.com/ 
+ * Options:
+ *  not implemented
  */
 
 (function(){
-liberator.commands.addUserCommand(['pukka','pu'], 'Post to Pukka',
+    var useNormalizelink = window.eval(liberator.globalVariables.pukka_normalizelink) || false;
+liberator.commands.addUserCommand(['pukka','pu'], 'Post to Pukka bookmark',
 function(args){
     if (!liberator.buffer.title || !liberator.buffer.URL || liberator.buffer.URL=='about:blank'){
         return false;
     }
-    var str = "pukka:";
+    var str = 'pukka:';
     var title = encodeURIComponent(liberator.buffer.title);
     var url = encodeURIComponent(liberator.buffer.URL);
+    var extend = liberator.buffer.getCurrentWord();
     if (args){
         url = encodeURIComponent(args);
     }
-    liberator.open(str + "url=" + url + "&title=" + title);
+    liberator.open(str + 'url=' + url + '&title=' + title + '&extended=' + extend);
 },{
-    usage: ['pukka [url] ','pu [url]'],
     completer: function(filter){
-        return [0, [getNormalizedPermalink(liberator.buffer.URL), "Normalize URL"]];
+        var complist = [];
+        if(useNormalizelink){
+            complist.push([getNormalizedPermalink(liberator.buffer.URL), 'Normalized URL']);
+        }
+        complist.push([liberator.buffer.URL, 'Raw URL']); 
+        return [0, complist];
     }
 }
 );
@@ -30,9 +49,12 @@ function(args){
 liberator.mappings.addUserMap([liberator.modes.NORMAL], 
 ['<C-z>'], 'Post to Pukka', 
 function() {
+    var urlarg = window.eval(liberator.globalVariables.pukka_normalizelink) ?
+                 getNormalizedPermalink(liberator.buffer.URL) :
+                 liberator.buffer.URL ;
     liberator.commandline.open(
         ':',
-        'pukka ' + getNormalizedPermalink(liberator.buffer.URL),
+        'pukka ' + urlarg,
         liberator.modes.EX
     );
 },{
