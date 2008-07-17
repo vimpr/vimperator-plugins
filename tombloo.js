30b81faf2abc10fe2ae100e49ed66f8ead2ff784
trapezoid <trapezoid@d0d07461-0603-4401-acd4-de1884942a52>
2008-07-17 07:19:15 +0000

    Tomblooとの連携プラグイン
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@15925 d0d07461-0603-4401-acd4-de1884942a52

---
+++ tombloo.js
@@ -0,0 +1,72 @@
+/**
+ * ==VimperatorPlugin==
+ * @name           tombloo.js
+ * @description    Tombloo integrate plugin
+ * @description-ja TomblooŒo—R‚Å‘I‘ð—Ìˆæ‚È‚Ç‚ðpost‚·‚é
+ * @author         Trapezoid
+ * @version        0.1
+ * ==/VimperatorPlugin==
+ *
+ * Usage:
+ * :tombloo arg         -> post by Tombloo
+ * :tomblooAction arg   -> execute Tombloo's action in tool menu
+ **/
+var TomblooService = Components.classes['@brasil.to/tombloo-service;1'].getService().wrappedJSObject;
+function update(target, src, keys){
+    if(keys){
+        keys.forEach(function(key){
+                target[key] = src[key];
+                });
+    } else {
+        for(var key in src)
+            target[key] = src[key];
+    }
+
+    return target;
+}
+
+function getContext(){
+    var doc = window.content.document;
+    var win = window.content.wrappedJSObject;
+    return update(update({
+        document  : doc,
+        window    : win,
+        title     : ''+doc.title || '',
+        selection : ''+win.getSelection(),
+        target    : doc,
+        //event     : event,
+        //mouse     : mouse,
+        //menu      : gContextMenu,
+    }, {}), win.location);
+}
+
+liberator.commands.addUserCommand(['tomblooAction'],'Execute Tombloo actions',
+    function(arg){
+        TomblooService.Tombloo.Service.actions[arg].execute();
+    },{
+        completer: function(filter){
+            var completionList = new Array();
+            for(var name in TomblooService.Tombloo.Service.actions)
+                if(name.indexOf(filter) > -1)
+                    completionList.push([name,name]);
+            return [0,completionList];
+        }
+    }
+);
+
+liberator.commands.addUserCommand(['tombloo'],'Post by Tombloo',
+    function(arg){
+        liberator.log(TomblooService.Tombloo.Service.extracters);
+        TomblooService.Tombloo.Service.share(getContext(), TomblooService.Tombloo.Service.extracters[arg],false);
+    },{
+        completer: function(filter){
+            var completionList = new Array();
+            var exts = TomblooService.Tombloo.Service.check(getContext());
+            liberator.log(exts[0]);
+            for(var i=0; i < exts.length; i++)
+                if(exts[i].name.indexOf(filter) > -1)
+                    completionList.push([exts[i].name,exts[i].name]);
+            return [0,completionList];
+        }
+    }
+);
