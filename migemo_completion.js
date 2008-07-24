359ae4bbaeb6d22270ce59102e9a85a75c2eab42
trapezoid <trapezoid@d0d07461-0603-4401-acd4-de1884942a52>
2008-07-24 11:48:03 +0000

    migemo_completion.js: initial release
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@16221 d0d07461-0603-4401-acd4-de1884942a52

---
+++ migemo_completion.js
@@ -0,0 +1,33 @@
+/**
+ * ==VimperatorPlugin==
+ * @name           migemo_completion.js
+ * @description    replace completion function with using Migemo
+ * @description-ja ï‚äÆä÷êîÇMigemoÇégópÇµÇΩÇ‡ÇÃÇ…éÊÇËë÷Ç¶ÇÈ
+ * @author         Trapezoid
+ * @version        0.1
+ * ==/VimperatorPlugin==
+ *
+ * Support commands:
+ *  - :buffer
+ **/
+var XMigemoCore;
+try{
+    XMigemoCore = Components.classes['@piro.sakura.ne.jp/xmigemo/factory;1']
+                            .getService(Components.interfaces.pIXMigemoFactory)
+                            .getService("ja");
+}
+catch(ex if ex instanceof TypeError){}
+
+function replaceFunction(target,symbol,f,originalArguments){
+    var oldFunction = target[symbol];
+    target[symbol] = function() f.apply(target,[oldFunction.apply(target,originalArguments || arguments), arguments]);
+}
+
+replaceFunction(liberator.completion,"buffer",function(oldResult,args){
+    var filter = args[0];
+    var migemoPattern = new RegExp(XMigemoCore.getRegExp(filter));
+    return [0, oldResult[1].filter(function([value,label]){
+        return migemoPattern.test(value) || migemoPattern.test(label)
+        })];
+},[""]);
+
