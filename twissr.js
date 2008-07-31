10f15b3d96a9631273613c7a37807a7e8f68e817
mattn <mattn@d0d07461-0603-4401-acd4-de1884942a52>
2008-07-31 04:40:00 +0000

    twitterとwassrにクロスポストできるvimperatorコマンド。ソース閲覧禁止
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@16909 d0d07461-0603-4401-acd4-de1884942a52

---
+++ twissr.js
@@ -0,0 +1,14 @@
+// Vimperator plugin: "Update Twitter & Wassr"
+// Last Change: 31-Jul-2008. Jan 2008
+// License: Public domain
+// Maintainer: mattn <mattn.jp@gmail.com> - http://mattn.kaoriya.net/
+
+(function(){
+    liberator.commands.addUserCommand(["twissr"], "Change Twitter & Wassr status",
+        function(arg, special){
+			arg = (special ? '! ' : ' ') + arg;
+			liberator.execute('twitter' + arg);
+			liberator.execute('wassr' + arg);
+        },
+    { });
+})();
