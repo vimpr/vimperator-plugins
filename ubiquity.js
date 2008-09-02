c8ed2b3c59cdbf9e0a8f0d96005345e2984354fd
mattn <mattn@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-02 09:23:12 +0000

    added ubiquity plugin for vimperator: 補完機能なし！
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@18648 d0d07461-0603-4401-acd4-de1884942a52

---
+++ ubiquity.js
@@ -0,0 +1,26 @@
+// Vimperator plugin: uaSwitch
+// Maintainer: mattn <mattn.jp@gmail.com> - http://mattn.kaoriya.net
+// Require: Ubiquity - https://wiki.mozilla.org/Labs/Ubiquity
+// Usage:
+//   :ubiquity command...  - show ubiquity's command dialog.
+//   :ubiquity! command... - run ubiquity's command.
+
+(function() {
+	if (typeof gUbiquity === 'undefined') return;
+    liberator.commands.addUserCommand(["ubiquity"], "Ubiquity",
+        function(arg, special){
+			var anchor = document.getElementById("content");
+			if (window.location == "chrome://browser/content/browser.xul")
+			    anchor = anchor.selectedBrowser;
+			gUbiquity.openWindow(anchor);
+			gUbiquity.__textBox.value = arg||'';
+			var context = gUbiquity.__makeContext();
+			var previewBlock = document.getElementById("cmd-preview").contentDocument.getElementById("preview");
+			gUbiquity.__cmdManager.updateInput("map osaka", context, previewBlock);
+			if (special) {
+				gUbiquity.__cmdManager.execute(context);
+				gUbiquity.closeWindow();
+			}
+		},
+    { });
+})();
