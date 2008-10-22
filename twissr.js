97846ee8f6a0aa833e22a42a65748d31db4ac4da
mattn <mattn@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-22 02:08:53 +0000

    follow latest vimperator.
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@21815 d0d07461-0603-4401-acd4-de1884942a52

--- twissr.js
+++ twissr.js
@@ -1,12 +1,12 @@
 // Vimperator plugin: "Update Twitter & Wassr"
-// Last Change: 31-Jul-2008. Jan 2008
+// Last Change: 20-Oct-2008. Jan 2008
 // License: Public domain
 // Maintainer: mattn <mattn.jp@gmail.com> - http://mattn.kaoriya.net/
 
 (function(){
-    liberator.commands.addUserCommand(["twissr"], "Change Twitter & Wassr status",
+    liberator.modules.commands.addUserCommand(["twissr"], "Change Twitter & Wassr status",
         function(arg, special){
-            arg = (special ? '! ' : ' ') + arg;
+            arg = (special ? '! ' : ' ') + arg.string;
             liberator.execute('twitter' + arg);
             liberator.execute('wassr' + arg);
         },
