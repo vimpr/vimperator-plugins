5a9596fb9bea00b07ebe9dc90d897e0262e1ac0c
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-09 10:40:53 +0000

    meow meow fixes.
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/branches/1.2@23054 d0d07461-0603-4401-acd4-de1884942a52

--- yetmappings.js
+++ yetmappings.js
@@ -25,14 +25,8 @@
   function exists (modes, key)
     (mappings.getCandidates(modes, key).length || mappings.get(modes, key));
 
-  function getYetMappings (pre) {
-    let result = [];
-    keys.forEach(function (key) {
-      if (!exists([modes.NORMAL], pre + key))
-        result.push(key);
-    });
-    return result;
-  }
+  function getYetMappings (pre)
+    keys.filter(function (key) (!exists([modes.NORMAL], pre + key)));
 
   commands.addUserCommand(
     ['yetmap[pings]', 'ymap'],
