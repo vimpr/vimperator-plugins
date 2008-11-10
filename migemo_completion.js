4aabb4dfa7e5a3ff3f3330ebbf12c17cf8eba9e0
drry <drry@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-10 06:52:20 +0000

    * `se fenc=utf-8`  * et cetera.
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@23116 d0d07461-0603-4401-acd4-de1884942a52

--- migemo_completion.js
+++ migemo_completion.js
@@ -2,7 +2,7 @@
  * ==VimperatorPlugin==
  * @name           migemo_completion.js
  * @description    replace completion function with using Migemo
- * @description-ja •âŠ®ŠÖ”‚ğMigemo‚ğg—p‚µ‚½‚à‚Ì‚Éæ‚è‘Ö‚¦‚é
+ * @description-ja è£œå®Œé–¢æ•°ã‚’Migemoã‚’ä½¿ç”¨ã—ãŸã‚‚ã®ã«å–ã‚Šæ›¿ãˆã‚‹
  * @author         Trapezoid
  * @version        0.2
  * ==/VimperatorPlugin==
@@ -18,24 +18,22 @@
  *  and more
  **/
 
-(function () {
-  var XMigemoCore = Components.classes['@piro.sakura.ne.jp/xmigemo/factory;1']
+(function(){
+  var XMigemoCore = Components.classes["@piro.sakura.ne.jp/xmigemo/factory;1"]
                               .getService(Components.interfaces.pIXMigemoFactory)
                               .getService("ja");
-  var XMigemoTextUtils = Components.classes['@piro.sakura.ne.jp/xmigemo/text-utility;1']
+  var XMigemoTextUtils = Components.classes["@piro.sakura.ne.jp/xmigemo/text-utility;1"]
                                    .getService(Components.interfaces.pIXMigemoTextUtils);
 
   function replaceFunction(target,symbol,f,originalArguments){
       var oldFunction = target[symbol];
-      target[symbol] = function() f.apply(target,[oldFunction.apply(target,originalArguments || arguments), arguments]);
+      target[symbol] = function() f.apply(target,[oldFunction.apply(target,originalArguments || arguments),arguments]);
   }
 
   replaceFunction(liberator.modules.completion,"buffer",function(oldResult,args){
       var filter = args[0];
       var migemoPattern = new RegExp(XMigemoCore.getRegExp(filter));
-      return [0, oldResult[1].filter(function([value,label]){
-          return migemoPattern.test(value) || migemoPattern.test(label)
-          })];
+      return [0,oldResult[1].filter(function([value,label]) migemoPattern.test(value) || migemoPattern.test(label))];
   },[""]);
 
   let original_filter = liberator.modules.completion.filter;
@@ -43,22 +41,21 @@
   liberator.modules.completion.filter = function(array,filter,matchFromBeginning){
       if(!filter) return array;
 
-      if (filter.match(/[()|]/))
-          return original_filter.apply(this, arguments);
+      if(/[()|]/.test(filter))
+          return original_filter.apply(this,arguments);
 
+      var migemoPattern;
       try {
-          var original = XMigemoTextUtils.sanitize(filter);
-          var migemoString = XMigemoCore.getRegExp(filter);
-          migemoString = original + '|' + migemoString;
+          let original = XMigemoTextUtils.sanitize(filter);
+          let migemoString = XMigemoCore.getRegExp(filter);
+          migemoString = original + "|" + migemoString;
           if(matchFromBeginning)
               migemoString ="^(" + migemoString + ")";
-          var migemoPattern = new RegExp(migemoString, 'i');
-      } catch (e) {
-          return original_filter.apply(this, arguments);
+          migemoPattern = new RegExp(migemoString,"i");
+      } catch(e) {
+          return original_filter.apply(this,arguments);
       }
 
-      return array.filter(function([value,label]){
-              return migemoPattern.test(value) || migemoPattern.test(label)
-          });
-  }
+      return array.filter(function([value,label]) migemoPattern.test(value) || migemoPattern.test(label));
+  };
 })();
