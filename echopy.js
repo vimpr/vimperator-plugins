6541b24d979e9e04d4c769db0b23bb3a827140b6
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-11 15:03:22 +0000

    Initial release: echo しつつ黒髪ロングへコピー
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@23255 d0d07461-0603-4401-acd4-de1884942a52

---
+++ echopy.js
@@ -0,0 +1,61 @@
+// ==VimperatorPlugin==
+// @name           echo and copy
+// @description    echo and copy
+// @description-ja echo and copy
+// @license        Creative Commons 2.1 (Attribution + Share Alike)
+// @version        1.0
+// @author         anekos (anekos@snca.net)
+// @maxVersion     2.0pre
+// @minVersion     2.0pre
+// ==/VimperatorPlugin==
+//
+// Usage:
+//    :echo <EXPRESSION>
+//      echo with copy (to clipboard).
+//
+// Usage-ja:
+//    :echo <EXPRESSION>
+//      echo するして同時にクリップボードにコピー
+//
+// Links:
+//    http://d.hatena.ne.jp/nokturnalmortum/20081111#1226414487
+
+(function () {
+
+  function neko (obj, useColor) {
+    switch (typeof obj) {
+      case 'object':
+        return liberator.modules.util.objectToString(obj, useColor);
+      case 'function':
+        return liberator.modules.util.escapeHTML(obj.toString());
+      case 'number':
+      case 'boolean':
+        return '' + obj;
+      case 'undefined':
+        return 'undefined';
+    }
+    return obj;
+  }
+
+  let echo = commands.get('echo');
+  let original_action = echo.action;
+
+  echo.action = function (arg, bang) {
+    if (bang) {
+      try {
+        if (arg.string == '')
+          return;
+        let obj = liberator.eval(arg.string);
+        liberator.echo(neko(obj, true));
+        liberator.modules.util.copyToClipboard(neko(obj, false));
+      } catch (e) {
+        liberator.echoerr(e);
+      }
+    } else {
+      original_action.apply(this, arguments);
+    }
+  };
+  echo.bang = true;
+
+
+})();
