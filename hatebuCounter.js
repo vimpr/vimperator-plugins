7f93f78f281d396b76b5da635bc40042bd2b1484
mattn <mattn@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-16 06:41:33 +0000

    はてなブックマーク 画像カウンタ
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@26849 d0d07461-0603-4401-acd4-de1884942a52

---
+++ hatebuCounter.js
@@ -0,0 +1,40 @@
+// Vimperator plugin: hatena bookmark image counter
+// Maintainer: mattn <mattn.jp@gmail.com> - http://mattn.kaoriya.net
+
+(function(){
+    const ICON = 'data:image/x-icon;base64,'+
+        'AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAQAQAAAAAAAAAAAAAAAAA'+
+        'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'+
+        'AAAAAAAAAAAAAAAAAAAAAOd5Uv/JXyr/yV8q/8lfKv/JXyr/yV8q/8lfKv/JXyr/yV8q/8lfKv/J'+
+        'Xyr/yV8q/8lfKv/JXyr/AAAAAAAAAAD6pYX/53lS/+d5Uv/neVL/53lS/+d5Uv/neVL/53lS/+d5'+
+        'Uv/neVL/53lS/+d5Uv/neVL/yV8q/wAAAAAAAAAA+qWF/+d5Uv/neVL/53lS/+d5Uv/neVL/53lS'+
+        '/+d5Uv/neVL/53lS/+d5Uv/neVL/53lS/8lfKv8AAAAAAAAAAPqlhf/neVL/53lS/+d5Uv//////'+
+        '///////////////////////////neVL/53lS/+d5Uv/JXyr/AAAAAAAAAAD6pYX/53lS/+d5Uv/n'+
+        'eVL////////////neVL/53lS/+d5Uv///////////+d5Uv/neVL/yV8q/wAAAAAAAAAA+qWF/+d5'+
+        'Uv/neVL/53lS////////////53lS/+d5Uv/neVL////////////neVL/53lS/8lfKv8AAAAAAAAA'+
+        'APqlhf/neVL/53lS/+d5Uv///////////+d5Uv/neVL/53lS////////////53lS/+d5Uv/JXyr/'+
+        'AAAAAAAAAAD6pYX/53lS/+d5Uv/neVL/////////////////////////////////53lS/+d5Uv/n'+
+        'eVL/yV8q/wAAAAAAAAAA+qWF/+d5Uv/neVL/53lS////////////53lS/+d5Uv///////////+d5'+
+        'Uv/neVL/53lS/8lfKv8AAAAAAAAAAPqlhf/neVL/53lS/+d5Uv///////////+d5Uv/neVL/////'+
+        '///////neVL/53lS/+d5Uv/JXyr/AAAAAAAAAAD6pYX/53lS/+d5Uv/neVL/////////////////'+
+        '///////////neVL/53lS/+d5Uv/neVL/yV8q/wAAAAAAAAAA+qWF/+d5Uv/neVL/53lS/+d5Uv/n'+
+        'eVL/53lS/+d5Uv/neVL/53lS/+d5Uv/neVL/53lS/8lfKv8AAAAAAAAAAPqlhf/neVL/53lS/+d5'+
+        'Uv/neVL/53lS/+d5Uv/neVL/53lS/+d5Uv/neVL/53lS/+d5Uv/JXyr/AAAAAAAAAAD6pYX/+qWF'+
+        '//qlhf/6pYX/+qWF//qlhf/6pYX/+qWF//qlhf/6pYX/+qWF//qlhf/6pYX/53lS/wAAAAAAAAAA'+
+        'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'+
+        'AAAA//8AAIABAACAAQAAgAEAAIABAACAAQAAgAEAAIABAACAAQAAgAEAAIABAACAAQAAgAEAAIAB'+
+        'AACAAQAA//8AAA==';
+    var hbCountIcon = document.createElement('statusbarpanel');
+    document.getElementById('status-bar').insertBefore(hbCountIcon, document.getElementById('security-button').nextSibling);
+    hbCountIcon.setAttribute('id', 'hatena-bookmark-count-icon');
+    hbCountIcon.setAttribute('src', ICON);
+    hbCountIcon.setAttribute('class', 'statusbarpanel-iconic');
+    hbCountIcon.addEventListener("click", function(e) {
+        liberator.open("http://b.hatena.ne.jp/entry/" + liberator.modules.buffer.URL, liberator.NEW_TAB);
+    }, false);
+    liberator.plugins.hbCountUpdate = function() {
+        hbCountIcon.setAttribute('src', "http://b.hatena.ne.jp/entry/image/" + liberator.modules.buffer.URL);
+    }
+    liberator.modules.autocommands.add('LocationChange', '.*', 'js liberator.plugins.hbCountUpdate()');
+})();
+// vim:sw=4 ts=4 et:
