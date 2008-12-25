99f6efcd0272472b3b1fe3b397a3041bca48a517
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-25 13:32:13 +0000

    License.
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@27409 d0d07461-0603-4401-acd4-de1884942a52

--- happy_hacking_vimperator.js
+++ happy_hacking_vimperator.js
@@ -1,23 +1,56 @@
-var PLUGIN_INFO =
+/*
+Copyright (c) 2008, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+*/
+
+let PLUGIN_INFO =
 <VimperatorPlugin>
-<name>Happy Happy Vimperator</name>
-<description>This plugin makes you to True Vimperatorer</description>
-<version>2.1</version>
-<author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
-<minVersion>2.0pre</minVersion>
-<maxVersion>2.0pre</maxVersion>
-<updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/happy_hacking_vimperator.js</updateURL>
-<detail><![CDATA[
-== Usage ==
-DO NOT THINK. FEEL!
-== Unbroken keyboard ==
-http://www.pfu.fujitsu.com/hhkeyboard/
-== Requirements ==
-Steel Heart
-== License ==
-Creative Commons Attribution-Share Alike 3.0 Unported
-http://creativecommons.org/licenses/by-sa/3.0/
-]]></detail>
+  <name>Happy Happy Vimperator</name>
+  <description>This plugin makes you to True Vimperatorer</description>
+  <version>2.2</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <minVersion>2.0pre</minVersion>
+  <maxVersion>2.0pre</maxVersion>
+  <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/happy_hacking_vimperator.js</updateURL>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <detail><![CDATA[
+    == Usage ==
+    DO NOT THINK. FEEL!
+    == Unbroken keyboard ==
+    http://www.pfu.fujitsu.com/hhkeyboard/
+    == Requirements ==
+    Steel Heart
+  ]]></detail>
 </VimperatorPlugin>;
 
 (function () {
