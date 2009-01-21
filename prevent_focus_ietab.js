78ac6d2d907f50dda8595f61b9426655b0054442
snaka <snaka@d0d07461-0603-4401-acd4-de1884942a52>
2009-01-21 12:08:48 +0000

    My first plugin
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@28759 d0d07461-0603-4401-acd4-de1884942a52

---
+++ prevent_focus_ietab.js
@@ -0,0 +1,23 @@
+/*
+
+prevent_focus_ietab.js
+
+Prevent focusing IE Tab when select a tab that renderd in IE Tab plugin.
+IE Tab に勝手にフォーカスを奪われて操作不能になるのを防ぐ
+
+Copyright (c) 2009, snaka<snaka.gml@gmail.com>.
+All rights reserved.
+This software distribute under term of new BSD style license.
+
+*/
+(function() {
+
+if (gIeTab && gIeTab.onTabSelected) {
+  liberator.log("replace IeTab.onTabSelected() function");
+  var func = gIeTab.onTabSelected.toSource();
+  var newFunc = func.replace(/window\.setTimeout\(gIeTab\.focusIeTab, 0\);/, '');
+  gIeTab.removeEventListener("appcontent", "select", gIeTab.onTabSelected);
+  gIeTab.addEventListener("appcontent", "select", new Function(newFunc));
+}
+
+})();
