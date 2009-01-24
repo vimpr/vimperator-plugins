d25530958326c8c4aa5455d6d6590380d1c7dd04
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-01-24 19:01:03 +0000

    PLUGIN_INFO 追加
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@28985 d0d07461-0603-4401-acd4-de1884942a52

--- migemized_find.js
+++ migemized_find.js
@@ -1,36 +1,86 @@
-// ==VimperatorPlugin==
-// @name           Migemized Find
-// @description-ja デフォルトのドキュメント内検索をミゲマイズする。
-// @license        Creative Commons 2.1 (Attribution + Share Alike)
-// @version        2.6
-// ==/VimperatorPlugin==
-//
-// Usage:
-//    検索ワードの一文字目が
-//      '/'  => 正規表現検索
-//      '?'  => Migemo検索
-//      以外 => Migemo検索
-//
-//    :ml <検索ワード> [-c <色>]
-//    :migelight <検索ワード> [-c <色>]
-//      検索ワードを指定色で強調表示する。
-//
-//    :ml! <色1> <色2> ... <色N>
-//    :migelight!  <色1> <色2> ... <色N>
-//      指定の色の強調表示を消す
-//
-//    :ml! all
-//    :migelight! all
-//      全ての強調表示を消す。
-//
-//    let g:migemized_find_language = "cat";
-//      ミ言語設定
-//
-// Author:
-//    anekos
-//
-// Link:
-//    http://d.hatena.ne.jp/nokturnalmortum/20080805#1217941126
+/* {{{
+Copyright (c) 2008, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+}}} */
+
+// PLUGIN_INFO {{{
+let PLUGIN_INFO =
+<VimperatorPlugin>
+  <name>Migemized Find</name>
+  <name lang="ja">Migemized Find</name>
+  <description>Migemize default page search.</description>
+  <description lang="ja">デフォルトのドキュメント内検索をミゲマイズする。</description>
+  <version>2.7</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <minVersion>2.0pre</minVersion>
+  <maxVersion>2.0pre</maxVersion>
+  <detail><![CDATA[
+    == Usage ==
+      検索ワードの一文字目が
+         '/'  => 正規表現検索
+         '?'  => Migemo検索
+         以外 => Migemo検索
+
+      検索ワードを指定色で強調表示する:
+        >||
+          :ml <検索ワード> [-c <色>]
+          :migelight <検索ワード> [-c <色>]
+        ||<
+
+      指定の色の強調表示を消す:
+        >||
+         :ml! <色1> <色2> ... <色N>
+         :migelight!  <色1> <色2> ... <色N>
+        ||<
+
+      全ての強調表示を消す:
+        >||
+          :ml! all
+          :migelight! all
+        ||<
+
+      ミ言語設定:
+        >||
+          let g:migemized_find_language = "cat";
+        ||<
+
+    == Link ==
+      http://d.hatena.ne.jp/nokturnalmortum/20080805#1217941126
+  ]]></detail>
+</VimperatorPlugin>;
+// }}}
+
 
 (function () {
 
