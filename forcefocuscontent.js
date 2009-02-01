9bcf030092b478d5b047060b2009453369ae4e4f
hogelog <hogelog@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-01 15:35:17 +0000

    add forcefocuscontent.js  * force focuscontent (nocheck focusedElement has value or not)
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@29400 d0d07461-0603-4401-acd4-de1884942a52

---
+++ forcefocuscontent.js
@@ -0,0 +1,23 @@
+// PLUGIN_INFO//{{{
+var PLUGIN_INFO =
+<VimperatorPlugin>
+    <name>{NAME}</name>
+    <description>force focuscontent</description>
+    <author mail="konbu.komuro@gmail.com" homepage="http://d.hatena.ne.jp/hogelog/">hogelog</author>
+    <version>0.1.0</version>
+    <minVersion>2.0pre</minVersion>
+    <maxVersion>2.0pre</maxVersion>
+    <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/forcefocuscontent.js</updateURL>
+</VimperatorPlugin>;
+//}}}
+
+getBrowser().addEventListener("load", onPageLoad, true);
+function onPageLoad(event)
+{
+    setTimeout(function () {
+        let focused = document.commandDispatcher.focusedElement;
+        if (focused)
+            focused.blur();
+    }, 100);
+}
+// vim: fdm=marker sw=4 ts=4 et:
