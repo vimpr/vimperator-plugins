8730e780f9caa0640ecfbcd1ad73e2f5c8e46be6
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-02 15:33:56 +0000

    色の補完を追加した
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@29451 d0d07461-0603-4401-acd4-de1884942a52

--- migemized_find.js
+++ migemized_find.js
@@ -39,7 +39,7 @@ let PLUGIN_INFO =
   <name lang="ja">Migemized Find</name>
   <description>Migemize default page search.</description>
   <description lang="ja">デフォルトのドキュメント内検索をミゲマイズする。</description>
-  <version>2.7</version>
+  <version>2.8</version>
   <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
   <license>new BSD License (Please read the source code comments of this plugin)</license>
   <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
@@ -87,6 +87,154 @@ let PLUGIN_INFO =
   let XMigemoCore = Components.classes['@piro.sakura.ne.jp/xmigemo/factory;1']
                      .getService(Components.interfaces.pIXMigemoFactory)
                      .getService(liberator.globalVariables.migemized_find_language || 'ja');
+
+  let colors = {
+    white: '#ffffff',
+    whitesmoke: '#f5f5f5',
+    ghostwhite: '#f8f8ff',
+    aliceblue: '#f0f8ff',
+    lavendar: '#e6e6fa',
+    azure: '#f0ffff',
+    lightcyan: '#e0ffff',
+    mintcream: '#f5fffa',
+    honeydew: '#f0fff0',
+    ivory: '#fffff0',
+    beige: '#f5f5dc',
+    lightyellow: '#ffffe0',
+    lightgoldenrodyellow: '#fafad2',
+    lemonchiffon: '#fffacd',
+    floralwhite: '#fffaf0',
+    oldlace: '#fdf5e6',
+    cornsilk: '#fff8dc',
+    papayawhite: '#ffefd5',
+    blanchedalmond: '#ffebcd',
+    bisque: '#ffe4c4',
+    snow: '#fffafa',
+    linen: '#faf0e6',
+    antiquewhite: '#faebd7',
+    seashell: '#fff5ee',
+    lavenderblush: '#fff0f5',
+    mistyrose: '#ffe4e1',
+    gainsboro: '#dcdcdc',
+    lightgray: '#d3d3d3',
+    lightsteelblue: '#b0c4de',
+    lightblue: '#add8e6',
+    lightskyblue: '#87cefa',
+    powderblue: '#b0e0e6',
+    paleturquoise: '#afeeee',
+    skyblue: '#87ceeb',
+    mediumaquamarine: '#66cdaa',
+    aquamarine: '#7fffd4',
+    palegreen: '#98fb98',
+    lightgreen: '#90ee90',
+    khaki: '#f0e68c',
+    palegoldenrod: '#eee8aa',
+    moccasin: '#ffe4b5',
+    navajowhite: '#ffdead',
+    peachpuff: '#ffdab9',
+    wheat: '#f5deb3',
+    pink: '#ffc0cb',
+    lightpink: '#ffb6c1',
+    thistle: '#d8bfd8',
+    plum: '#dda0dd',
+    silver: '#c0c0c0',
+    darkgray: '#a9a9a9',
+    lightslategray: '#778899',
+    slategray: '#708090',
+    slateblue: '#6a5acd',
+    steelblue: '#4682b4',
+    mediumslateblue: '#7b68ee',
+    royalblue: '#4169e1',
+    blue: '#0000ff',
+    dodgerblue: '#1e90ff',
+    cornflowerblue: '#6495ed',
+    deepskyblue: '#00bfff',
+    cyan: '#00ffff',
+    aqua: '#00ffff',
+    turquoise: '#40e0d0',
+    mediumturquoise: '#48d1cc',
+    darkturquoise: '#00ced1',
+    lightseagreen: '#20b2aa',
+    mediumspringgreen: '#00fa9a',
+    springgreen: '#00ff7f',
+    lime: '#00ff00',
+    limegreen: '#32cd32',
+    yellowgreen: '#9acd32',
+    lawngreen: '#7cfc00',
+    chartreuse: '#7fff00',
+    greenyellow: '#adff2f',
+    yellow: '#ffff00',
+    gold: '#ffd700',
+    orange: '#ffa500',
+    darkorange: '#ff8c00',
+    goldenrod: '#daa520',
+    burlywood: '#deb887',
+    tan: '#d2b48c',
+    sandybrown: '#f4a460',
+    darksalmon: '#e9967a',
+    lightcoral: '#f08080',
+    salmon: '#fa8072',
+    lightsalmon: '#ffa07a',
+    coral: '#ff7f50',
+    tomato: '#ff6347',
+    orangered: '#ff4500',
+    red: '#ff0000',
+    deeppink: '#ff1493',
+    hotpink: '#ff69b4',
+    palevioletred: '#db7093',
+    violet: '#ee82ee',
+    orchid: '#da70d6',
+    magenta: '#ff00ff',
+    fuchsia: '#ff00ff',
+    mediumorchid: '#ba55d3',
+    darkorchid: '#9932cc',
+    darkviolet: '#9400d3',
+    blueviolet: '#8a2be2',
+    mediumpurple: '#9370db1',
+    gray: '#808080',
+    mediumblue: '#0000cd',
+    darkcyan: '#008b8b',
+    cadetblue: '#5f9ea0',
+    darkseagreen: '#8fbc8f',
+    mediumseagreen: '#3cb371',
+    teal: '#008080',
+    forestgreen: '#228b22',
+    seagreen: '#2e8b57',
+    darkkhaki: '#bdb76b',
+    peru: '#cd853f',
+    crimsin: '#dc143c',
+    indianred: '#cd5c5c',
+    rosybrown: '#bc8f8f',
+    mediumvioletred: '#c71585',
+    dimgray: '#696969',
+    black: '#000000',
+    midnightblue: '#191970',
+    darkslateblue: '#483d8b',
+    darkblue: '#00008b',
+    navy: '#000080',
+    darkslategray: '#2f4f4f',
+    green: '#008000',
+    darkgreen: '#006400',
+    darkolivegreen: '#556b2f',
+    olivedrab: '#6b8e23',
+    olive: '#808000',
+    darkgoldenrod: '#b8860b',
+    chocolate: '#d2691e',
+    sienna: '#a0522d',
+    saddlebrown: '#8b4513',
+    firebrick: '#b22222',
+    brown: '#a52a2a',
+    maroon: '#800000',
+    darkred: '#8b0000',
+    darkmagenta: '#8b008b',
+    purple: '#800080',
+    indigo: '#4b0082',
+  };
+
+  let colorsCompltions = [
+    [name, <span style={'color: ' + name}>{'\u25a0 ' + value}</span>]
+    for each ([name, value] in Iterator(colors))
+  ];
 
   function getPosition (elem) {
     if (!elem)
@@ -461,13 +609,13 @@ let PLUGIN_INFO =
       } else {
         let r = MF.highlightAll(args.join(' '), args['-color']);
         liberator.echo(r ? r.length + ' words migelighted.'
-               : 'word not found.');
+                         : 'word not found.');
       }
     },
     {
       bang: true,
       options: [
-        [['-color', '-c'], commands.OPTION_STRING],
+        [['-color', '-c'], commands.OPTION_STRING, null, colorsCompltions],
       ]
     }
   );
@@ -484,7 +632,7 @@ let PLUGIN_INFO =
         return MF.removeHighlight(MF.highlightColor);
       if (args == 'all')
         return [f() for each (f in MF.storage.highlightRemovers)];
-      args.split(/\s+/).forEach(MF.removeHighlight);
+      args.split(/\s+/).forEach(MF.removeHighlight, MF);
     }
   );
 
@@ -499,7 +647,7 @@ let PLUGIN_INFO =
     {
       options: [
         [['-backward', '-b'], commands.OPTION_NOARG],
-        [['-color', '-c'], commands.OPTION_STRING],
+        [['-color', '-c'], commands.OPTION_STRING, null, colorsCompltions],
       ]
     }
   );
