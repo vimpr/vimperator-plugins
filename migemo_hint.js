ee82861df012d3995775374913aaa82c87fddeeb
secondlife <secondlife@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-05 13:24:25 +0000

    - lang/javascript/vimperator-plugins/migemo_hint.js migemo の正規表現マッチを空白区切りで AND 検索するように
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@29585 d0d07461-0603-4401-acd4-de1884942a52

--- migemo_hint.js
+++ migemo_hint.js
@@ -1,5 +1,5 @@
 // Vimperator plugin: 'Hint Matching with XUL/Migemo'
-// Last Change: 29-Oct-2008. Jan 2008
+// Last Change: 05-Feb-2009. Jan 2008
 // License: Creative Commons
 // Maintainer: Trapezoid <trapezoid.g@gmail.com> - http://unsigned.g.hatena.ne.jp/Trapezoid
 // Require: XUL/Migemo extension - https://addons.mozilla.org/ja/firefox/addon/5239
@@ -8,14 +8,16 @@
 //
 // Usage:
 //  :set hintmatching = custom
-liberator.plugins.customHintMatcher = function(inputString){
+lberator.plugins.customHintMatcher = function(inputString){
     var XMigemoCore;
     try{
-        XMigemoCore = Components.classes['@piro.sakura.ne.jp/xmigemo/factory;1']
+        XMigemoCore = Cc['@piro.sakura.ne.jp/xmigemo/factory;1']
                                 .getService(Components.interfaces.pIXMigemoFactory)
                                 .getService("ja");
+        XMigemoTextUtils = Cc['@piro.sakura.ne.jp/xmigemo/text-utility;1']
+                                .getService(Ci.pIXMigemoTextUtils);
     }
     catch(ex if ex instanceof TypeError){}
-    var r = new RegExp(XMigemoCore.getRegExp(inputString));
+    var r = new RegExp(XMigemoTextUtils.getANDFindRegExpFromTerms(XMigemoCore.getRegExps(inputString)), 'gi');
     return function(hintString) r.test(hintString);
 }
