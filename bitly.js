a60c9705cf21146739477c0e7bb24c4692e8e73f
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-08 12:38:40 +0000

    PLUGIN_INFO 対応 引数も取れるようにした
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@29711 d0d07461-0603-4401-acd4-de1884942a52

--- bitly.js
+++ bitly.js
@@ -1,12 +1,58 @@
-// ==VimperatorPlugin==
-// @name           Bit.ly
-// @description-ja Bit.ly で短縮URLを得る
-// @license        Creative Commons 2.1 (Attribution + Share Alike)
-// @version        1.0
-// @minVersion     1.2
-// @maxVersion     2.0pre
-// ==/VimperatorPlugin==
-//
+/* NEW BSD LICENSE {{{
+Copyright (c) 2008, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+}}} */
+
+// PLUGIN_INFO {{{
+let PLUGIN_INFO =
+<VimperatorPlugin>
+  <name>bit.ly</name>
+  <description>Get short alias by bit.ly</description>
+  <description lang="ja">Bit.ly で短縮URLを得る</description>
+  <version>1.1.0</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/bitly.js</updateURL>
+  <minVersion>1.2</minVersion>
+  <maxVersion>2.0pre</maxVersion>
+  <detail><![CDATA[
+    == Commands ==
+      :bitly [<URL>]
+        Copy to clipboard.
+  ]]></detail>
+</VimperatorPlugin>;
+// }}}
+
 
 (function () {
 
@@ -28,11 +74,14 @@
   commands.addUserCommand(
     ['bitly'],
     'Copy bitly url',
-    function () {
-      bitly(buffer.URL, function (short) {
+    function (args) {
+      bitly(args.literalArg || buffer.URL, function (short) {
         util.copyToClipboard(short);
         liberator.echo('`' + short + "' was copied to clipboard.");
       });
+    },
+    {
+      literal: 0
     },
     true
   );
