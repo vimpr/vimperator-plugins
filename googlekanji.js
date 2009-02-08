b80a867b776257459e0a0c34aa243d0348d7689c
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-08 11:59:04 +0000

    HEAD 対応＆ PLUGIN_INFO 対応
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@29705 d0d07461-0603-4401-acd4-de1884942a52

--- googlekanji.js
+++ googlekanji.js
@@ -1,17 +1,62 @@
-// ==VimperatorPlugin==
-// @name           Google-Kanji
-// @description-ja グーグルを使って漢字を検索
-// @license        Creative Commons 2.1 (Attribution + Share Alike)
-// @version        1.1
-// ==/VimperatorPlugin==
-//
-// Usage:
-//    :gkanji うぶめ
-//    のようにひらがななどで読みを入力します。
-//    すると、
-//    :gkcopy
-//    が開き、補完が可能になるので、正しそうな漢字を選びます。
-//    すると、クリップボードにその漢字がコピーされます。
+/* NEW BSD LICENSE {{{
+Copyright (c) 2008, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+}}} */
+
+// PLUGIN_INFO {{{
+let PLUGIN_INFO =
+<VimperatorPlugin>
+  <name>Google Kanji</name>
+  <name lang="ja">Google 漢字</name>
+  <description>Search kanji by google</description>
+  <description lang="ja">グーグルを使って漢字を検索</description>
+  <version>1.1.2</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/googlekanji.js</updateURL>
+  <minVersion>2.0pre</minVersion>
+  <maxVersion>2.0pre</maxVersion>
+  <detail><![CDATA[
+    == Usage ==
+      :gkanji うぶめ
+      のようにひらがななどで読みを入力します。
+      すると、
+      :gkcopy
+      が開き、補完が可能になるので、正しそうな漢字を選びます。
+      すると、クリップボードにその漢字がコピーされます。
+  ]]></detail>
+</VimperatorPlugin>;
+// }}}
 
 (function () {
 
@@ -79,11 +124,12 @@
     {
       completer: function (context) {
         context.title = ['kanji', 'count'];
-        context.items = copycompl;
+        context.completions = copycompl;
       }
     },
     true
   );
 
-
 })();
+
+// vim:sw=2 ts=2 et si fdm=marker:
