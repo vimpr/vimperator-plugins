6a1d6440d5b5641bca23e7566739b363dbaa3d51
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-03-02 15:04:55 +0000

    名称をひっそり変更
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@30711 d0d07461-0603-4401-acd4-de1884942a52

--- command_menu.js
+++ command_menu.js
@@ -35,10 +35,10 @@ THE POSSIBILITY OF SUCH DAMAGE.
 // PLUGIN_INFO {{{
 let PLUGIN_INFO =
 <VimperatorPlugin>
-  <name>Command-MainMenu</name>
+  <name>Command Menu</name>
   <description>Execute main-menu and tool-bar by ex-command.</description>
   <description lang="ja">メインメニューとツールバーをコマンドで実行できる</description>
-  <version>1.5.1</version>
+  <version>1.5.2</version>
   <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
   <license>new BSD License (Please read the source code comments of this plugin)</license>
   <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
