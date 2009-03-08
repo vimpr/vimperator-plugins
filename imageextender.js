e8e8f352700d92178a4d22514ff718ced60318ce
janus_wel <janus_wel@d0d07461-0603-4401-acd4-de1884942a52>
2009-03-08 11:36:40 +0000

    Apply patch from Frank
    Add features about zoom.
    Note that existing keybind to save image is replace to ';M'.
    
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@30951 d0d07461-0603-4401-acd4-de1884942a52

--- imageextender.js
+++ imageextender.js
@@ -39,12 +39,12 @@ let PLUGIN_INFO =
     <description lang="ja">画像操作特集。</description>
     <author mail="janus_wel@fb3.so-net.ne.jp" homepage="http://d.hatena.ne.jp/janus_wel">janus_wel</author>
     <license document="http://www.opensource.org/licenses/bsd-license.php">New BSD License</license>
-    <version>0.11</version>
+    <version>0.20</version>
     <minversion>2.0pre</minversion>
     <maxversion>2.0pre</maxversion>
     <detail><![CDATA[
 == USAGE ==
-Extended-hints mode ';m' to yank image URL and ';i' to save image are
+Extended-hints mode ';m' to yank image URL and ';M' to save image are
 available. These are default settings. You can change these by below
 settings.
 
@@ -52,11 +52,31 @@ Ex-command ':downimageall' is also available. This is to download all
 images of current page, but it effects heavy load to the server, you must
 use carefully. We are NOT RESPONSIBLE for result of this command.
 
+Additionally, following modes are available if you installed Image Zoom
+add-on ( https://addons.mozilla.org/de/firefox/addon/139 ). These feature
+were provided by Frank Blendinger. Thanks !!
+
+- ';i' to zoom in image
+- ';x' to zoom out image
+- ';X' to zoom reset image
+- ';z' to zoom fit image
+- ';Z' to custom zoom image
+
 == SETTING ==
 :image_extender_yank_key:
 The key to yank image URL(default: 'm').
 :image_extender_save_key:
-The key to save image(default: 'i').
+The key to save image(default: 'M').
+:image_extender_zoom_in_key:
+The key to zoom in image(default: 'i').
+:image_extender_zoom_out_key:
+The key to zoom out image(default: 'x').
+:image_extender_zoom_reset_key:
+The key to zoom reset image(default: 'X').
+:image_extender_zoom_fit_key:
+The key to zoom fit image(default: 'z').
+:image_extender_zoom_custom_key:
+The key to custom zoom image(default: 'Z').
 :image_extender_skip_prompt:
 If this setted 'true', skip prompt to locate and name. Changing this value
 will reflect dynamically. ':downimageall' command ignore this
@@ -76,18 +96,37 @@ start it to save image, and prompt is not displayed at save operation.
 ]]></detail>
     <detail lang="ja"><![CDATA[
 == USAGE ==
-画像の URL をヤンクする ';m' と 画像を保存する ';i' という拡張ヒントモードが使えるようになります。
+画像の URL をヤンクする ';m' と 画像を保存する ';M' という拡張ヒントモードが使えるようになります。
 これらはデフォルト設定です。後述する設定でキーを変更できます。
 
 ':downimageall' という ex コマンドも使えるようになります。
 これは現在のページの画像をすべて保存するものですが、サーバに大きな負荷がかかるため注意して使ってください。
 このコマンドの使用による結果は一切の責任を負いかねます。
 
+さらに、 Image Zoom ( https://addons.mozilla.org/de/firefox/addon/139 ) アドオンをインストールしている場合以下のモードが使えるようになります。
+この機能は Frank Blendinger さんによって提供されました。ありがとう !!
+
+- 画像を拡大する ';i'
+- 画像を縮小する ';x'
+- 画像の拡大縮小を元に戻す ';X'
+- 画像を画面にあわせて表示する ';z'
+- 画像をカスタムズームさせる ';Z'
+
 == SETTING ==
 :image_extender_yank_key:
 画像 の URL をヤンクするキーです (デフォルト: 'm') 。
 :image_extender_save_key:
-画像を保存するキーです (デフォルト: 'i') 。
+画像を保存するキーです (デフォルト: 'M') 。
+:image_extender_zoom_in_key:
+画像を拡大するキーです (デフォルト: 'i') 。
+:image_extender_zoom_out_key:
+画像を縮小するキーです (デフォルト: 'x') 。
+:image_extender_zoom_reset_key:
+画像の拡大縮小を元に戻すキーです (デフォルト: 'X') 。
+:image_extender_zoom_fit_key:
+画像を画面にあわせて拡大縮小するキーです (デフォルト: 'z') 。
+:image_extender_zoom_custom_key:
+画像をカスタムズームさせるキーです (デフォルト: 'Z') 。
 :image_extender_skip_prompt:
 この値が 'true' の場合、保存する場所や名前を指定するダイアログは表示されません。
 この値の変更は即座に反映されます。
@@ -110,8 +149,13 @@ image_skip_prompt='true'
 ( function () {
 
 // default settings
-const yankKey = liberator.globalVariables.image_extender_yank_key || 'm';
-const saveKey = liberator.globalVariables.image_extender_save_key || 'i';
+const yankKey       = liberator.globalVariables.image_extender_yank_key         || 'm';
+const saveKey       = liberator.globalVariables.image_extender_save_key         || 'M';
+const zoomInKey     = liberator.globalVariables.image_extender_zoom_in_key      || 'i';
+const zoomOutKey    = liberator.globalVariables.image_extender_zoom_out_key     || 'x';
+const zoomResetKey  = liberator.globalVariables.image_extender_zoom_reset_key   || 'X';
+const zoomFitKey    = liberator.globalVariables.image_extender_zoom_fit_key     || 'z';
+const zoomCustomKey = liberator.globalVariables.image_extender_zoom_custom_key  || 'Z';
 
 // common settings
 const query = '//img[@src and not(starts-with(@src, "data:"))]';
@@ -137,6 +181,63 @@ hints.addMode(
     },
     function () query
 );
+if (window.izImage) {
+    // to zoom in image
+    hints.addMode(
+        zoomInKey,
+        'Zoom in image',
+        function (element) {
+            var oizImage = new izImage(element);
+            oizImage.zoom(nsIPrefBranchObj.getIntPref("zoomvalue")/100);
+            reportStatus(oizImage);
+        },
+        function () query
+    );
+    // to zoom out image
+    hints.addMode(
+        zoomOutKey,
+        'Zoom out image',
+        function (element) {
+            var oizImage = new izImage(element);
+            oizImage.zoom(100/nsIPrefBranchObj.getIntPref("zoomvalue"));
+            reportStatus(oizImage);
+        },
+        function () query
+    );
+    // to zoom reset image
+    hints.addMode(
+        zoomResetKey,
+        'Zoom reset image',
+        function (element) {
+            var oizImage = new izImage(element);
+            oizImage.setZoom(100);
+            reportStatus(oizImage);
+        },
+        function () query
+    );
+    // to zoom fit image
+    hints.addMode(
+        zoomFitKey,
+        'Zoom fit image',
+        function (element) {
+            var oizImage = new izImage(element);
+            oizImage.fit(nsIPrefBranchObj.getBoolPref("autocenter"));
+            reportStatus(oizImage);
+        },
+        function () query
+    );
+    // to custom zoom image
+    hints.addMode(
+        zoomCustomKey,
+        'Custom zoom image',
+        function (element) {
+            var oizImage = new izImage(element);
+            openDialog("chrome://imagezoom/content/customzoom.xul", "", "chrome,modal,centerscreen", "Image", oizImage);
+            reportStatus(oizImage);
+        },
+        function () query
+    );
+}
 commands.addUserCommand(
     ['downimageall'],
     'download all images of current page',
