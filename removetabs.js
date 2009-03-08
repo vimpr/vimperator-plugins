9dbf54d36978607b0d79fec8f437e3305f1910be
fifnel <fifnel@d0d07461-0603-4401-acd4-de1884942a52>
2009-03-08 01:04:02 +0000

    タブをまとめて閉じるプラグイン
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@30941 d0d07461-0603-4401-acd4-de1884942a52

---
+++ removetabs.js
@@ -0,0 +1,56 @@
+var PLUGIN_INFO =
+<VimperatorPlugin>
+<name>removetabs</name>
+<description>RemoveTabs</description>
+<description lang="ja">タブをまとめて閉じる</description>
+<author mail="fifnel@gmail.com" homepage="http://fifnel.com/">fifnel</author>
+<version>0.1</version>
+<minVersion>2.0pre</minVersion>
+<maxVersion>2.0pre</maxVersion>
+<updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/removetabs.js</updateURL>
+<detail lang="ja"><![CDATA[
+これはremovetabsアドオンと似たような処理を行うアドオンです。
+現在のタブから左（もしくは右）のタブをすべてクローズすることができます。
+https://addons.mozilla.org/ja/firefox/addon/4227
+
+== Command ==
+
+:removetabsleft:
+    現在のタブから左にあるタブをすべて閉じます。
+    現在のタブは閉じられません。
+    
+:removetabsright:
+    現在のタブから右にあるタブをすべて閉じます。
+    現在のタブは閉じられません。
+
+== 設定例 ==
+.vimperatorrcに以下のような感じで設定すると良いかもしれません。
+>||
+    noremap <C-P> :removetabsleft<CR>
+    noremap <C-N> :removetabsright<CR>
+||<
+]]></detail>
+</VimperatorPlugin>;
+
+(function(){
+    liberator.modules.commands.addUserCommand(['removetabsleft'], 'remove tabs left',
+        function() {
+            var ts = getBrowser().tabContainer.childNodes;
+            var ct = getBrowser().selectedTab;
+            var i;
+            for( i=ts.length-1; ts[i]!=ct; i-- ) {}
+            for( i--; i>=0; i-- ) {
+                getBrowser().removeTab( ts[i] );
+            }
+        },{}
+    );
+    liberator.modules.commands.addUserCommand(['removetabsright'], 'remove tabs right',
+        function(){
+            var ts = getBrowser().tabContainer.childNodes;
+            var ct = getBrowser().selectedTab;
+            for( var i=ts.length-1; ts[i]!=ct; i-- ) {
+                getBrowser().removeTab( ts[i] );
+            }
+        },{}
+    );
+})();
