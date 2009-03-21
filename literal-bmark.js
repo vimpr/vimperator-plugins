8610581fc551aea3c95dc1f870beea9ec0563606
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-03-21 08:35:11 +0000

    Initial release: ブックマークレットなど用の :bmark コマンド
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@31428 d0d07461-0603-4401-acd4-de1884942a52

---
+++ literal-bmark.js
@@ -0,0 +1,86 @@
+/* NEW BSD LICENSE {{{
+Copyright (c) 2009, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+}}} */
+
+// PLUGIN_INFO {{{
+let PLUGIN_INFO =
+<VimperatorPlugin>
+  <name>literal bmark</name>
+  <description>:bmark command that can included characters such as '"' in the URL.</description>
+  <description lang="ja">'"' などの文字を含めることができる :bmark コマンド</description>
+  <version>1.0.0</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/literal-bmark.js</updateURL>
+  <minVersion>2.0pre</minVersion>
+  <maxVersion>2.0pre</maxVersion>
+  <detail><![CDATA[
+    == Commands ==
+      :lbmark:
+        The arguments are the same as :bmark command.
+        But, in this command, characters such as '"' can be included in the URL.
+        However, there should be the options (e.g. -title=...) before the URL.
+  ]]></detail>
+  <detail lang="ja"><![CDATA[
+    == Commands ==
+      :lbmark:
+        引数は :bmark コマンドと同じですが、URL に '"' などの文字を含めることが出来ます。
+        ただし、-title などのオプションは URL に先行する必要があります。
+  ]]></detail>
+</VimperatorPlugin>;
+// }}}
+
+(function () {
+
+  let bmark = commands.get('bmark');
+
+  commands.addUserCommand(
+    ['lbm[ark]'],
+    'Add a bookmark - literal',
+    bmark.action,
+    {
+      literal: 0,
+      bang: bmark.bang,
+      count: bmark.count,
+      argCount: '1',
+      options: bmark.options,
+      complete: bmark.complete
+    },
+    true
+  );
+
+
+})();
+
+// vim:sw=2 ts=2 et si fdm=marker:
