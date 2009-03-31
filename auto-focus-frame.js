cecf0f7be18a7d9777f287a0232d6aa7bd6f2384
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-03-31 17:03:48 +0000

    Initial release: 最も大きなフレームに自動的にフォーカスするプラグイン
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@31752 d0d07461-0603-4401-acd4-de1884942a52

---
+++ auto-focus-frame.js
@@ -0,0 +1,86 @@
+/* NEW BSD LICENSE {{{
+Copyright (c) 2009, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+}}} */
+
+// PLUGIN_INFO {{{
+let PLUGIN_INFO =
+<VimperatorPlugin>
+  <name>Auto focus frame</name>
+  <description>Automatically focus to largest frame.</description>
+  <description lang="ja">最も大きなフレームに自動的にフォーカスする。</description>
+  <version>1.0.0</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/auto-focus-frame.js</updateURL>
+  <minVersion>2.0</minVersion>
+  <maxVersion>2.1pre</maxVersion>
+  <detail><![CDATA[
+    == Usage ==
+      Only install.
+  ]]></detail>
+  <detail lang="ja"><![CDATA[
+    == Usage ==
+      インストールするだけ
+  ]]></detail>
+</VimperatorPlugin>;
+// }}}
+
+(function () {
+
+  autocommands.add(
+    'DOMLoad',
+    '.*',
+    function () {
+      if (!(window.content.document instanceof HTMLDocument))
+          return;
+      if (content.frames.length <= 1)
+        return;
+      let [maxSize, maxFrame] = [-1, content.frames[0]];
+      for (let frame in util.Array.iterator(content.frames)) {
+        let size = frame.innerHeight * frame.innerWidth;
+        win = window.content;
+        if (win.scrollMaxX <= 0 && win.scrollMaxY <= 0)
+            continue;
+        if (maxSize < size) {
+          maxSize = size;
+          maxFrame = frame;
+        }
+      }
+      maxFrame.focus();
+    }
+  );
+
+})();
+
+// vim:sw=2 ts=2 et si fdm=marker:
