7908576244379abf408dab0ef5fc2736d5a74352
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-04-15 11:52:33 +0000

    Initial release: 自動でwildoptions=auto をオンオフ
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@32457 d0d07461-0603-4401-acd4-de1884942a52

---
+++ auto-wildoptions.js
@@ -0,0 +1,88 @@
+/* NEW BSD LICENSE {{{
+Copyright (c) 2009, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+}}} */
+
+// PLUGIN_INFO {{{
+let PLUGIN_INFO =
+<VimperatorPlugin>
+  <name>Auto wildoptions</name>
+  <description>Automatically toggle wildoptions=auto</description>
+  <description lang="ja">自動的に wildoptions=auto をオンオフします。</description>
+  <version>1.0.0</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/auto-wildoptions.js</updateURL>
+  <minVersion>2.0</minVersion>
+  <maxVersion>2.1pre</maxVersion>
+  <detail><![CDATA[
+    ----
+  ]]></detail>
+  <detail lang="ja"><![CDATA[
+    ----
+  ]]></detail>
+</VimperatorPlugin>;
+// }}}
+
+(function () {
+
+  function around (obj, name, func) {
+    let next = obj[name];
+    obj[name] = function ()
+      let (self = this, args = arguments)
+        func.call(self,
+                  function () next.apply(self, args),
+                  args);
+  }
+
+  let context = mappings.getDefault(modes.NORMAL, 'g<').action;
+  around(
+    liberator.eval('Completions', context).prototype.complete,
+    'complete',
+    function (next, args) {
+      options.get('wildoptions').set('auto');
+      return next();
+    }
+  );
+  around(
+    commandline,
+    'open',
+    function (next, args) {
+      options.get('wildoptions').set('');
+      return next();
+    }
+  );
+
+})();
+
+// vim:sw=2 ts=2 et si fdm=marker:
