e0b332c3883ecd46db0004918b166be0adee5a97
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2009-04-16 15:48:16 +0000

    util.js の変更に対応
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@32503 d0d07461-0603-4401-acd4-de1884942a52

--- exShowElementInfo.js
+++ exShowElementInfo.js
@@ -4,7 +4,7 @@
  * @description     extend feature that show element's information when extended-hints mode ";?"
  * @description-ja  extended-hints mode の ";?" でみられる要素の情報を拡張する。
  * @author          janus_wel <janus_wel@fb3.so-net.ne.jp>
- * @version         0.10
+ * @version         0.11
  * @minversion      2.0pre
  * ==/VimperatorPlugin==
  *
@@ -16,6 +16,7 @@
  *
  * HISTORY
  *   2008/11/05 ver. 0.10   - initial written.
+ *   2009/04/17 ver. 0.11   - follow the util.js changes
  *
  * */
 
@@ -26,7 +27,7 @@ addFeatureToMethodAfter(
     'showElementInfo',
     function (element) {
         let str = [
-            a.name + ': ' + a.value for (a in liberator.modules.util.Array.iterator(element.attributes))
+            a.name + ': ' + a.value for (a in liberator.modules.util.Array.itervalues(element.attributes))
         ].join("\n");
         liberator.echo("\nextra information\n" + str, liberator.modules.commandline.APPEND_TO_MESSAGES);
     }
