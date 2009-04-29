3d012a061424199504407c651b5122523fc46a28
drry <drry@d0d07461-0603-4401-acd4-de1884942a52>
2009-04-29 13:40:23 +0000

    * oops. [32720]
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@32949 d0d07461-0603-4401-acd4-de1884942a52

--- twitter.js
+++ twitter.js
@@ -64,7 +64,6 @@
         xhr.open("GET", "https://twitter.com/statuses/user_timeline/" + user + ".json?count=1", false, username, password);
         xhr.send(null);
         xhr.open("POST", "https://twitter.com/favourings/create/" + window.eval(xhr.responseText)[0].id + ".json", false, username, password);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.send(null);
     }
     function unfavTwitter(username, password, user){
@@ -72,7 +71,6 @@
         xhr.open("GET", "https://twitter.com/statuses/user_timeline/" + user + ".json?count=1", false, username, password);
         xhr.send(null);
         xhr.open("DELETE", "https://twitter.com/favourings/destroy/" + window.eval(xhr.responseText)[0].id + ".json", false, username, password);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.send(null);
     }
     function showTwitterMentions(username, password){
@@ -102,12 +100,12 @@
         liberator.echo(html, true);
     }
     function showFollowersStatus(username, password, target){
+        // for debug
+        //target = "otsune"
         var xhr = new XMLHttpRequest();
         var endPoint = target ? "https://twitter.com/statuses/user_timeline/" + target + ".json"
             : "https://twitter.com/statuses/friends_timeline.json";
         xhr.open("GET", endPoint, false, username, password);
-        // for debug
-        //xhr.open("GET", "https://twitter.com/statuses/user_timeline/otsune.json", false, username, password);
         xhr.send(null);
         var statuses = evalFunc(xhr.responseText) || [];
 
