26d3bdab37bf7260520f7138835a9e0d09945f50
suVene <suVene@d0d07461-0603-4401-acd4-de1884942a52>
2009-06-14 03:46:52 +0000

    version 上げ忘れ
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@33964 d0d07461-0603-4401-acd4-de1884942a52

--- _libly.js
+++ _libly.js
@@ -12,7 +12,7 @@ var PLUGIN_INFO =
     <description lang="ja">適当なライブラリっぽいものたち。</description>
     <author mail="suvene@zeromemory.info" homepage="http://zeromemory.sblo.jp/">suVene</author>
     <license>MIT</license>
-    <version>0.1.22</version>
+    <version>0.1.23</version>
     <minVersion>1.2</minVersion>
     <maxVersion>2.0pre</maxVersion>
     <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/_libly.js</updateURL>
