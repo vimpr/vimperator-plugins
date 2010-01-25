aa298f73ba1c5c181bb20c1e670c13b7f4dcf598
hogelog <hogelog@d0d07461-0603-4401-acd4-de1884942a52>
2010-01-25 14:20:30 +0000

    * tabcommands.js: add tab commands  * :lock, :protect, :freeze, :ren[ame], :dup[licate]
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@36531 d0d07461-0603-4401-acd4-de1884942a52

---
+++ tabcommands.js
@@ -0,0 +1,59 @@
+// PLUGIN_INFO//{{{
+var PLUGIN_INFO =
+<VimperatorPlugin>
+    <name>{NAME}</name>
+    <description>add some tab commands</description>
+    <author mail="konbu.komuro@gmail.com" homepage="http://d.hatena.ne.jp/hogelog/">hogelog</author>
+    <version>0.0.1</version>
+    <minVersion>2.2</minVersion>
+    <maxVersion>2.2</maxVersion>
+    <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/tabcommands.js</updateURL>
+    <detail><![CDATA[
+== COMMANDS ==
+dup[licate]:
+    duplicate current tab
+ren[ame]:
+    rename current tab
+freeze:
+    freeze (protect&lock) current tab
+protect:
+    protect current tab
+lock:
+    lock current tab
+
+]]></detail>
+</VimperatorPlugin>;
+//}}}
+(function(){
+commands.add(["dup[licate]"], "duplicate current tab", function(args) {
+    gBrowser.duplicateTab(gBrowser.mCurrentTab);
+});
+commands.add(["ren[ame]"], "rename current tab", function(args) {
+    gBrowser.renameTab(gBrowser.mCurrentTab);
+});
+commands.add(["freeze"], "freeze current tab", function(args) {
+    let protect = gBrowser.mCurrentTab.hasAttribute("protected");
+    let lock = gBrowser.mCurrentTab.hasAttribute("locked");
+    if (protect && lock) {
+        gBrowser.mCurrentTab.removeAttribute("protected");
+        gBrowser.mCurrentTab.removeAttribute("locked");
+    } else 
+        gBrowser.freezeTab(gBrowser.mCurrentTab);
+});
+commands.add(["protect"], "protect current tab", function(args) {
+    let protect = gBrowser.mCurrentTab.hasAttribute("protected");
+    if (protect)
+        gBrowser.mCurrentTab.removeAttribute("protected");
+    else
+        gBrowser.mCurrentTab.setAttribute("protected", true);
+});
+commands.add(["lock"], "lock current tab", function(args) {
+    let lock = gBrowser.mCurrentTab.hasAttribute("locked");
+    if (lock)
+        gBrowser.mCurrentTab.removeAttribute("locked");
+    else
+        gBrowser.mCurrentTab.setAttribute("locked", true);
+});
+
+})();
+// vim: fdm=marker sw=4 ts=4 et:
