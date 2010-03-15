455c8eed3c89d6c743c6a0e0ff17e59adbd5ded4
snaka <snaka@d0d07461-0603-4401-acd4-de1884942a52>
2010-03-15 14:37:03 +0000

    version++
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@37032 d0d07461-0603-4401-acd4-de1884942a52

--- _libly.js
+++ _libly.js
@@ -12,7 +12,7 @@ var PLUGIN_INFO =
     <description lang="ja">適当なライブラリっぽいものたち。</description>
     <author mail="suvene@zeromemory.info" homepage="http://zeromemory.sblo.jp/">suVene</author>
     <license>MIT</license>
-    <version>0.1.31</version>
+    <version>0.1.32</version>
     <minVersion>2.3pre</minVersion>
     <maxVersion>2.3pre</maxVersion>
     <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/_libly.js</updateURL>
