210fda90f564ec537f39d8511a20dec59296541b
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2010-05-11 12:04:47 +0000

    Init
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@37450 d0d07461-0603-4401-acd4-de1884942a52

---
+++ foxage2ch.js
@@ -0,0 +1,165 @@
+/* NEW BSD LICENSE {{{
+Copyright (c) 2010, anekos.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. The names of the authors may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
+
+###################################################################################
+# http://sourceforge.jp/projects/opensource/wiki/licenses%2Fnew_BSD_license       #
+# に参考になる日本語訳がありますが、有効なのは上記英文となります。                #
+###################################################################################
+
+}}} */
+
+// PLUGIN_INFO {{{
+let PLUGIN_INFO =
+<VimperatorPlugin>
+  <name>FoxAge2ch</name>
+  <description>for FoxAge2ch</description>
+  <version>1.0.0</version>
+  <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
+  <license>new BSD License (Please read the source code comments of this plugin)</license>
+  <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
+  <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/foxage2ch.js</updateURL>
+  <minVersion>2.4</minVersion>
+  <maxVersion>2.4</maxVersion>
+  <detail><![CDATA[
+    for FoxAge2ch
+  ]]></detail>
+  <detail lang="ja"><![CDATA[
+    for FoxAge2ch
+  ]]></detail>
+</VimperatorPlugin>;
+// }}}
+// INFO {{{
+let INFO =
+<>
+  <plugin name="FoxAge2ch" version="1.0.0"
+          href="http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/foxage2ch.js"
+          summary="for FoxAge2ch addon"
+          lang="en-US"
+          xmlns="http://vimperator.org/namespaces/liberator">
+    <author email="anekos@snca.net">anekos</author>
+    <license>New BSD License</license>
+    <project name="Vimperator" minVersion="2.4"/>
+    <p></p>
+    <item>
+      <tags>:faopen</tags>
+      <spec>:fao<oa>pen</oa></spec>
+      <description><p></p></description>
+    </item>
+  </plugin>
+  <plugin name="FoxAge2ch" version="1.0.0"
+          href="http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/foxage2ch.js"
+          summary="FoxAge2ch アドオン用"
+          lang="ja"
+          xmlns="http://vimperator.org/namespaces/liberator">
+    <author email="anekos@snca.net">anekos</author>
+    <license>New BSD License</license>
+    <project name="Vimperator" minVersion="2.4"/>
+    <p></p>
+    <item>
+      <tags>:faopen</tags>
+      <spec>:fao<oa>pen</oa></spec>
+      <description><p></p></description>
+    </item>
+  </plugin>
+</>;
+// }}}
+
+
+(function () {
+
+  Components.utils.import("resource://foxage2ch/utils.jsm");
+  let svc = FoxAge2chUtils.service;
+
+  const Status = {
+    Live: 0,
+    New: 2,
+    Dead: 4
+  };
+
+  const StatusIcon = {
+    0: '\u2601', // 曇
+    2: '\u2600', // 晴
+    4: '\u2602', // 雨
+  }
+
+  function selType1 (board)
+    (board.type === 1);
+
+  let threads =
+    let (last, time = 0) (
+      function () {
+        if (last)
+          return last;
+        last = util.Array.flatten([
+          svc.getChildItems(board.id, {})
+          for ([, board] in Iterator(svc.getChildItems("root", {}).filter(selType1)))
+        ]);
+        return last;
+      }
+    );
+
+  commands.addUserCommand(
+    ['fao[pen]'],
+    'Open the borard from FoxAge2ch',
+    function (args) {
+      let thread = threads()[parseInt(args.literalArg.replace(/^.\s/, ''), 10) - 1];
+      if (!thread)
+        return;
+      liberator.log(thread);
+      liberator.open(FoxAge2chUtils.parseToURL(thread), liberator.NEW_TAB);
+    },
+    {
+      literal: 0,
+      options: [
+        [['-live', '-l'], commands.OPTION_NOARG],
+        [['-dead', '-d'], commands.OPTION_NOARG],
+        [['-new', '-n'], commands.OPTION_NOARG],
+      ],
+      completer: function (context, args) {
+        context.completions = [
+          [
+            let (icon = StatusIcon[thread.status])
+            (icon ? icon + ' ' : '') + (idx + 1) + ': ' + thread.title,
+            thread.id
+          ]
+          for ([idx, thread] in Iterator(threads()))
+          if (
+            (!args['-live'] || thread.status === Status.Live)
+            &&
+            (!args['-dead'] || thread.status === Status.Dead)
+            &&
+            (!args['-new'] || thread.status === Status.New)
+          )
+        ];
+      }
+    },
+    true
+  );
+
+})();
+
+// vim:sw=2 ts=2 et si fdm=marker:
