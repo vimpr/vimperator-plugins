86f2e73a75c3b678e72b0829723e19e76504ec9e
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2010-08-07 15:07:44 +0000

    Version++
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@38219 d0d07461-0603-4401-acd4-de1884942a52

--- twittperator.js
+++ twittperator.js
@@ -28,7 +28,7 @@ let PLUGIN_INFO =
   <name>twittperator</name>
   <description>Twitter Client using ChirpStream</description>
   <description lang="ja">OAuth対応Twitterクライアント</description>
-  <version>1.0.2</version>
+  <version>1.0.3</version>
   <minVersion>2.3</minVersion>
   <maxVersion>2.4</maxVersion>
   <author mail="teramako@gmail.com" homepage="http://d.hatena.ne.jp/teramako/">teramako</author>
