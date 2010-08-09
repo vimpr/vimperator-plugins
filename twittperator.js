70638e61d24bc0951ab04e22c219d69636c47b2a
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2010-08-09 12:24:24 +0000

    リンク補完時に RT が候補にならないバグを修正
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@38236 d0d07461-0603-4401-acd4-de1884942a52

--- twittperator.js
+++ twittperator.js
@@ -28,7 +28,7 @@ let PLUGIN_INFO =
   <name>twittperator</name>
   <description>Twitter Client using ChirpStream</description>
   <description lang="ja">OAuth対応Twitterクライアント</description>
-  <version>1.0.8.0.0.0.0.0.0.0.0.0.0.0.1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1</version>
+  <version>1.0.9</version>
   <minVersion>2.3</minVersion>
   <maxVersion>2.4</maxVersion>
   <author mail="teramako@gmail.com" homepage="http://d.hatena.ne.jp/teramako/">teramako</author>
@@ -1540,7 +1540,7 @@ function loadPlugins() { // {{{
       },
       link: function(context, args) {
         context.completions =
-          history.filter(function(s) /https?:\/\//(s.text)).map(rt(function(s) [s.text, s]));
+          history.map(rt(function(s) [s.text, s])).filter(function([t]) /https?:\/\//(t));
       },
       text: function(context, args) {
         context.completions =
