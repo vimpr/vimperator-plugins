e582448b367250a10098460ecc7567642a70ad68
anekos <anekos@d0d07461-0603-4401-acd4-de1884942a52>
2010-10-15 14:56:28 +0000

    へんこうにたいおう
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk@38587 d0d07461-0603-4401-acd4-de1884942a52

--- gmail-commando.js
+++ gmail-commando.js
@@ -39,7 +39,7 @@ let PLUGIN_INFO =
   <name lang="ja">GMail コマンドー</name>
   <description>The handy commands for GMail</description>
   <description lang="ja">便利なGMail用コマンドー</description>
-  <version>1.4.0</version>
+  <version>1.4.1</version>
   <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
   <license>new BSD License (Please read the source code comments of this plugin)</license>
   <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
@@ -76,7 +76,7 @@ let g:gmail_commando_label_shortcut = 1
 // INFO {{{
 let INFO =
 <>
-  <plugin name="GMailCommando" version="1.3.7"
+  <plugin name="GMailCommando" version="1.4.1"
           href="http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/gmail-commando.js"
           summary="The handy commands for GMail"
           lang="en-US"
@@ -113,7 +113,7 @@ let INFO =
       </description>
     </item>
   </plugin>
-  <plugin name="GMailコマンドー" version="1.3.7"
+  <plugin name="GMailコマンドー" version="1.4.1"
           href="http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/gmail-commando.js"
           summary="便利なGMail用コマンドー"
           lang="ja"
@@ -272,7 +272,7 @@ let INFO =
 
     get labels() A(this.doc.querySelectorAll('a.n0')).filter(function (it) (/#label/(it.href))),
 
-    get input() this.doc.getElementById(':rh'),
+    get input() this.doc.getElementById(':rf'),
 
     get searchButton() this.doc.getElementById(':rj'),
 
