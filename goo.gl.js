6bc0bd669eb78b88059bdb5fb9bf8b1a3ae073b4
mitsugu oyama <mitsugu.oyama@gmail.com>
2010-10-25 20:22:52 +0900

    remove PLUGIN_INFO and MIT License Document, and modified plugin's URL

--- goo.gl.js
+++ goo.gl.js
@@ -1,31 +1,7 @@
-/* The MIT License {{{
-
-Copyright (c) 2010, mitsugu oyama
-Copyright (c) 2010, anekos
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-}}} */
-
 // INFO //
 var INFO = 
 <plugin name="goo.gl.js" version="0.1"
-        href="http://gist.github.com/630237"
+        href="http://github.com/vimpr/vimperator-plugins/blob/master/goo.gl.js"
         summary="goo.gl.js"
         xmlns="http://vimperator.org/namespaces/liberator">
   <author email="mitsugu.oyama@gmail.com">Mitsugu Oyama</author>
@@ -44,43 +20,21 @@ var INFO =
   </item>
 </plugin>;
 
-// PLUGIN_INFO//{{{
-var PLUGIN_INFO =
-<VimperatorPlugin>
-    <name>googl</name>
-    <description>Get short URL from Google</description>
-    <author mail="mitsugu.oyama@gmail.com" homepage="http://myscript.zouri.jp/">mitsugu oyama</author>
-    <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
-    <version>0.0.1</version>
-    <minVersion>2.0pre</minVersion>
-    <maxVersion>2.4pre</maxVersion>
-    <detail><![CDATA[
-
-        == Commands ==
-            googl:
-                :googl [long-URL]
-
-        == Note ==
-
-    ]]></detail>
-</VimperatorPlugin>;
-//}}}
-
 (function(){
-	let short_url=function(long_url){
-		let req=new XMLHttpRequest();
-		req.addEventListener("load",function(){
-			let response=JSON.parse(req.responseText);
-			liberator.echo(response.short_url);
-			util.copyToClipboard(response.short_url,true);
-		},false);
-		req.addEventListener("error",function(){
-			liberator.echo("Responce errror status from goo.gl. Status Code:" + req.status);
-		},false);
-		req.open("POST", "http://goo.gl/api/shorten?url="+encodeURIComponent(long_url));
-		req.setRequestHeader("X-Auth-Google-Url-Shortener","true");
-		req.send();
-	};
+  let short_url=function(long_url){
+    let req=new XMLHttpRequest();
+    req.addEventListener("load",function(){
+      let response=JSON.parse(req.responseText);
+      liberator.echo(response.short_url);
+      util.copyToClipboard(response.short_url,true);
+    },false);
+    req.addEventListener("error",function(){
+      liberator.echo("Responce errror status from goo.gl. Status Code:" + req.status);
+    },false);
+    req.open("POST", "http://goo.gl/api/shorten?url="+encodeURIComponent(long_url));
+    req.setRequestHeader("X-Auth-Google-Url-Shortener","true");
+    req.send();
+  };
   commands.addUserCommand(
     ["googl"],
     "Get short URL from Google",
