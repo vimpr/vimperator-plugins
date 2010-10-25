e1442683f0e0590781b9570fe03ff221a4f74c4c
mitsugu oyama <mitsugu.oyama@gmail.com>
2010-10-25 19:56:10 +0900

    remove PLUGIN_INFO and MIT License Document, and modified plugin's URL

--- gmap.js
+++ gmap.js
@@ -1,31 +1,8 @@
-/* The MIT License {{{
-
-Copyright (c) 2010 Mitsugu Oyama
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-}}} */
-
 // INFO //
 var INFO = 
 <plugin name="gmap.js" version="0.1"
         summary="gmap.js"
-        href="http://gist.github.com/635593"
+        href="http://github.com/vimpr/vimperator-plugins/blob/master/gmap.js"
         xmlns="http://vimperator.org/namespaces/liberator">
   <author email="mitsugu.oyama@gmail.com">Mitsugu Oyama</author>
   <license href="http://opensource.org/licenses/mit-license.php">MIT</license>
@@ -48,34 +25,6 @@ var INFO =
     </description>
   </item>
 </plugin>;
-
-// PLUGIN_INFO {{{
-let PLUGIN_INFO =
-<VimperatorPlugin>
-  <name>gmap</name>
-  <name lang="ja">gmap</name>
-  <description>Get Google Map URL of current location</description>
-  <description lang="ja">現在位置のGoogle Map上でのURLを取得します。</description>
-  <version>0.1</version>
-  <author mail="mitsugu.oyama@gmail.com" homepage="http://myscript.zouri.jp">mitsugu oyama</author>
-  <license>MIT License (Please read the source code comments of this plugin)</license>
-  <license lang="ja">MITライセンス (ソースコードのコメントを参照してください)</license>
-  <minVersion>2.3.1</minVersion>
-  <maxVersion>2.4pre</maxVersion>
-  <detail lang="ja"><![CDATA[
-		== Commands ==
-			:gmap
-				現在位置のGoogle Map上でのURLを取得しクリップボードにYankします。
-
-		== Note ==
-				詳細は以下のURLを参照してください。なおXPCOM経由でgeolocation機能を
-				利用しているため、プライバシーの確認が行われません。御注意ください。
-					http://mozilla.jp/firefox/features/geolocation/
-					https://developer.mozilla.org/Ja/Using_geolocation
-
-  ]]></detail>
-</VimperatorPlugin>;
-// }}}
 
 (function(){
 	commands.addUserCommand(
