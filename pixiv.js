8322d1063659c16870b0a8431a9ed1d8cc3cdefc
mitsugu oyama <mitsugu.oyama@gmail.com>
2010-10-31 20:45:19 +0900

    add href attribute in plugin element

--- pixiv.js
+++ pixiv.js
@@ -2,6 +2,7 @@
 var INFO = 
 <plugin name="pixiv.js" version="0.1"
         summary="pixiv.js"
+        href="http://github.com/vimpr/vimperator-plugins/blob/master/pixiv.js"
         xmlns="http://vimperator.org/namespaces/liberator">
   <author email="mitsugu.oyama@gmail.com">Mitsugu Oyama</author>
   <license href="http://opensource.org/licenses/mit-license.php">MIT</license>
