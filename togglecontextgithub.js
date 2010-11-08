81e8285e0cb3160e5ca2b6a464e1e8213a7cc9e2
mitsugu oyama <mitsugu.oyama@gmail.com>
2010-11-08 17:00:46 +0900

    modified summary

--- togglecontextgithub.js
+++ togglecontextgithub.js
@@ -1,7 +1,7 @@
 // INFO //
 var INFO = 
 <plugin name="togglecontextgithub.js" version="0.1"
-        summary="togglecontextgithub.js"
+        summary="Toggle context exchange box on github"
         href="http://github.com/vimpr/vimperator-plugins/blob/master/togglecontextgithub.js"
         xmlns="http://vimperator.org/namespaces/liberator">
   <author email="mitsugu.oyama@gmail.com">Mitsugu Oyama</author>
