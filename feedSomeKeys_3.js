3acf9c35a8bd63d821107801e1125dc097a89d6b
anekos <anekos@snca.net>
2010-11-11 04:51:17 +0900

    Fix: domain regex

--- feedSomeKeys_3.js
+++ feedSomeKeys_3.js
@@ -39,7 +39,7 @@ let PLUGIN_INFO =
   <name lang="ja">feedSomeKeys 3</name>
   <description>feed some defined key events into the Web content</description>
   <description lang="ja">キーイベントをWebコンテンツ側に送る</description>
-  <version>1.8.2</version>
+  <version>1.8.3</version>
   <author mail="anekos@snca.net" homepage="http://d.hatena.ne.jp/nokturnalmortum/">anekos</author>
   <license>new BSD License (Please read the source code comments of this plugin)</license>
   <license lang="ja">修正BSDライセンス (ソースコードのコメントを参照してください)</license>
@@ -79,7 +79,7 @@ lazy fmaps -u='http://code.google.com/p/vimperator-labs/issues/detail' u
 // }}}
 // INFO {{{
 let INFO = <>
-  <plugin name="feedSomeKeys" version="1.8.2"
+  <plugin name="feedSomeKeys" version="1.8.3"
           href="http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/feedSomeKeys_3.js"
           summary="Feed some defined key events into the Web content"
           lang="en-US"
@@ -181,7 +181,7 @@ let INFO = <>
 :lazy fmaps -u='http://code.google.com/p/vimperator-labs/issues/detail' u
     </ex></code>
   </plugin>
-  <plugin name="feedSomeKeys" version="1.8.2"
+  <plugin name="feedSomeKeys" version="1.8.3"
           href="http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/feedSomeKeys_3.js"
           summary="Web コンテンツに直接キーイベントを送ります。"
           lang="ja"
@@ -515,7 +515,7 @@ let INFO = <>
       ];
       if (currentURL) {
         result.unshift(['^' + util.escapeRegex(buffer.URL), 'Current URL']);
-        result.unshift(['^' + util.escapeRegex(content.document.domain), 'Current domain']);
+        result.unshift([util.escapeRegex(content.document.domain), 'Current domain']);
       }
       return result;
     };
