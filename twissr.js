da3680fb80f098a7fb0390aa2391a623d2e43385
laiso <laiso.org@gmail.com>
2010-11-14 18:43:39 +0900

    use :tw(twittperator.js)

--- twissr.js
+++ twissr.js
@@ -1,5 +1,5 @@
 // Vimperator plugin: "Update Twitter & Wassr"
-// Last Change: 20-Oct-2008. Jan 2008
+// Last Change: 14-Nov-2010. Jan 2008
 // License: Public domain
 // Maintainer: mattn <mattn.jp@gmail.com> - http://mattn.kaoriya.net/
 
@@ -7,7 +7,7 @@
     liberator.modules.commands.addUserCommand(["twissr"], "Change Twitter & Wassr status",
         function(arg){
             arg = (arg.bang ? '! ' : ' ') + arg.string;
-            liberator.execute('twitter' + arg);
+            liberator.execute('tw' + arg);
             liberator.execute('wassr' + arg);
         },
     { });
