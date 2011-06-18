dd01081890d25934d5f5f296e4680ee090484012
aereal <aereal.h@gmail.com>
2011-06-19 01:10:06 +0900

    fixed complement sub diaries

--- access_hatena.js
+++ access_hatena.js
@@ -133,7 +133,7 @@ map ; :accesshatena
         root.containerOpen = true;
         for (var i = 0, length = root.childCount; i < length; i++) {
             var page = root.getChild(i);
-            page.uri.match('^https?://([a-zA-Z0-9.]+)\\.hatena\\.ne\\.jp/([a-zA-Z][a-zA-Z0-9_-]{1,30}[a-zA-Z0-9]/?)?');
+            page.uri.match('^https?://([a-zA-Z0-9.]+)\\.hatena\\.ne\\.jp/([a-zA-Z][a-zA-Z0-9_-]{1,30}[a-zA-Z0-9](?:\\+[a-zA-Z0-9_-]{1,30})?/?)?');
             var host = RegExp.$1;
             var id   = RegExp.$2;
             var _recent_hosts_length = recentHosts.length;
