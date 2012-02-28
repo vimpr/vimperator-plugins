de64efc7ddff6b91bb6fab5fb19ed3f4d6274282
whatsthebeef <whiskeytangobravo@gmail.com>
2012-02-28 10:23:21 -0600

    neatened up description

--- google-results.js
+++ google-results.js
@@ -1,8 +1,8 @@
 /**
 * ==VimperatorPlugin==
 * @name google-results.js
-* @description helps to select google results
-* @author john bower (whiskeytangobravo@gmail.com, http://www.whatsthebeef.org)
+* @description provides quick access to google results
+* @author whatsthebeef (whiskeytangobravo@gmail.com, http://www.whatsthebeef.org)
 * @version 0.1
 * @minversion 2.0pre
 * ==/VimperatorPlugin==
@@ -11,8 +11,15 @@
 * Creative Commons
 *
 * USAGE
-*
+* :gr [index]
+* index - the index of the result you would like to access
+* 
 * EXAMPLE
+* keys can be mapped like so
+* nmap ,1 :gr<Space>0<CR> 
+* nmap ,2 :gr<Space>1<CR> 
+* etc...
+* the you only need to press ,1 to access first result...
 *
 * HISTORY
 * 2012/02/27 ver. 0.1 - initial written.
