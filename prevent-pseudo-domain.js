8eb51b16d0497d3170acdccf7bb571b51630a196
anekos <anekos@snca.net>
2012-06-17 18:20:31 +0900

    No pee.

--- prevent-pseudo-domain.js
+++ prevent-pseudo-domain.js
@@ -1,28 +1,28 @@
 // ORIGINAL: http://vimperator.g.hatena.ne.jp/kei-s/20101005
 (function() {
-    hasMultipleWords = function(s) {
+    function hasMultipleWords (s) {
         return s.split(/\s/).length > 1;
     }
 
-    isWord = function(s) {
+    function isWord (s) {
         return /^[^\s\.]+$/i.test(s);
     }
 
-    isIPAddress = function(s) {
+    function isIPAddress (s) {
         return /^([0-9]+\.){3}([0-9]+)$/i.test(s);
     }
 
-    hasScheme = function(s) {
+    function hasScheme (s) {
         return /^([a-zA-Z0-9-]+):/i.test(s);
     }
 
     let tldList = "local|museum|travel|aero|arpa|coop|info|jobs|name|nvus|biz|com|edu|gov|int|mil|net|org|pro|xxx|ac|ad|ae|af|ag|ai|ak|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|ct|cu|cv|cx|cy|cz|dc|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fl|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hi|hk|hm|hn|hr|ht|hu|ia|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|ks|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mi|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|nd|ne|nf|ng|nh|ni|nj|nl|nm|no|np|nr|nu|ny|nz|oh|ok|om|or|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ri|ro|ru|rw|sa|sb|sc|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tx|tz|ua|ug|uk|um|us|ut|uy|uz|va|vc|ve|vg|vi|vn|vt|vu|wa|wf|wi|ws|wv|wy|ye|yt|yu|za|zm|zw".split("|");
-    hasTLD = function(s) {
+    function hasTLD (s) {
         let m = s.match(/^(?:(?:[^\.\s:\/]+\.)+?)+([a-zA-Z]+)(?::\d+)?(?:\/[^\s]*)?$/i);
         return m != null && tldList.indexOf(m[1]) != -1;
     }
 
-    requirePrefix = function(s) {
+    function requirePrefix (s) {
         if (!s.trim())
           return false;
         let validators = [hasMultipleWords, isWord, isIPAddress, hasScheme, hasTLD];
