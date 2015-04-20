9ef5e617d2bce18ebc90ba394c2b0f223a044842
anekos <anekos@snca.net>
2015-04-20 22:39:39 +0900

    オリジナルのアクションを使う

--- google-exopen.js
+++ google-exopen.js
@@ -11,20 +11,24 @@ let PLUGIN_INFO = xml`
 // }}}
 
 (function () {
+    let original = mappings.getDefault(modes.NORMAL, 'o');
+
     mappings.addUserMap(
       [modes.NORMAL],['o'],':open',
       function() {
         // urlを取得
         var url = window.content.window.location;
-        // コマンドの引数
-        var commandPram = '';
         // google検索か判定
-        if(url.host === 'www.google.co.jp') {
-          // クエリ部の抜き出し
-          var q = decodeURI(url.href).match(/q=(.*?)&/);
-          // foo+bar+hogeの形で取得されるので'+'を' 'で置き換え
-          var commandPram = q[1].replace(/\+/g,' ');
+        if(url.host !== 'www.google.co.jp') {
+          return original.action.apply(this, arguments);
         }
+
+        // クエリ部の抜き出し
+        var q = decodeURI(url.href).match(/q=(.*?)&/);
+        // コマンドの引数
+        // foo+bar+hogeの形で取得されるので'+'を' 'で置き換え
+        var commandPram = q[1].replace(/\+/g,' ');
+
         // コマンドの生成
         var command = 'open ' + commandPram;
         commandline.open('',
