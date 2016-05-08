7b0d2785ffd1956d7e33a2650d37920faa0acbf5
akameco <akameco.t@gmail.com>
2016-05-08 18:20:01 +0900

    Update format

--- google-exopen.js
+++ google-exopen.js
@@ -11,34 +11,24 @@ let PLUGIN_INFO = xml`
 // }}}
 
 (function () {
-    let original = mappings.getDefault(modes.NORMAL, 'o');
+	const original = mappings.getDefault(modes.NORMAL, 'o');
 
-    mappings.addUserMap(
-      [modes.NORMAL],['o'],':open',
-      function() {
-        // urlを取得
-        var url = window.content.window.location;
-        // google検索か判定
-        if(url.host !== 'www.google.co.jp') {
-          return original.action.apply(this, arguments);
-        }
+	mappings.addUserMap([modes.NORMAL], ['o'], ':open', () => {
+		// urlを取得
+		const url = window.content.window.location;
+		// google検索か判定
+		if (url.host !== 'www.google.co.jp') {
+			return original.action.apply(this, arguments);
+		}
 
-        // クエリ部の抜き出し
-        var q = url.href.match(/[?&]q=(.*?)&/);
-        // コマンドの引数
-        // foo+bar+hogeの形で取得されるので'+'を' 'で置き換え
-        var commandPram = decodeURIComponent(q[1]).replace(/\+/g,' ');
+		// クエリ部の抜き出し
+		const q = url.href.match(/[?&]q=(.*?)&/);
+		// コマンドの引数
+		// foo+bar+hogeの形で取得されるので'+'を' 'で置き換え
+		const commandPram = decodeURIComponent(q[1]).replace(/\+/g, ' ');
 
-        // コマンドの生成
-        var command = 'open ' + commandPram;
-        commandline.open('',
-          commands.commandToString(
-            {
-              command: command
-            }
-        ),modes.EX);
-      }
-    );
+		// コマンドの生成
+		const command = 'open ' + commandPram;
+		commandline.open('', commands.commandToString({command: command}), modes.EX);
+	});
 })();
-
-// vim:sw=2 ts=2 et si fdm=marker:
