7a9ec6cdc2ce94f2498e31b3442530f96c9aea3f
Amlitzer <amlitzer@users.noreply.github.com>
2016-09-08 21:09:02 +0200

    Firefox compatibility fixes

--- jquery-loader.js
+++ jquery-loader.js
@@ -81,7 +81,7 @@ let INFO = xml`
       get document () content.document.wrappedJSObject,
       get location () content.window.location.wrappedJSObject
     };
-    liberator.eval(File(filepath).read(), context);
+    liberator.eval(io.File(filepath).read(), context);
     return context.window.jQuery;
   };
 
