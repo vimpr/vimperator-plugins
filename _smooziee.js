vimpr/vimperator-plugins

commit 9f78aa3af10568a1ed85000168d1189a261e064a

Fix issues with plugin reload.
Using 'let' for PLUGIN_INFO/INFO instead of 'var' causes:

  SyntaxError: redeclaration of non-configurable global property
  PLUGIN_INFO/INFO

Cf. :h plugin-documentation which uses 'var' as well.

_smooziee.js
@@ -78,7 +78,8 @@ var INFO = xml`
 </plugin>`;
 // }}}
 
-let self = liberator.plugins.smooziee = (function(){
+liberator.plugins.smooziee = (function(){
+  const self = this;
 
   // Mappings  {{{
   mappings.addUserMap(

commit 11fd68c406da78141b87d24258eb31717bae28d0

fix `TypeError: self.smoothScrollBy is not a function`
9f78aa3af10568a1ed85000168d1189a261e064a broke `self`'s scope.

_smooziee.js
@@ -78,9 +78,7 @@ var INFO = xml`
 </plugin>`;
 // }}}
 
-liberator.plugins.smooziee = (function(){
-  const self = this;
-
+let self = liberator.plugins.smooziee = (function(){
   // Mappings  {{{
   mappings.addUserMap(
     [modes.NORMAL],